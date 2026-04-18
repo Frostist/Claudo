a0165699d259236184bb3622ed818176d6dd3d8e
Frostist <37545822+Frostist@users.noreply.github.com>
2026-04-18 17:28:50 +0200

    feat: add base NPC scene with speech bubble and exported properties

---
+++ scenes/npc/npc.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var npc_name: String = "Unknown"
+@export var npc_texture: Texture2D
+
+@onready var sprite: Sprite2D = $NPCSprite
+
+func _ready() -> void:
+	if npc_texture:
+		sprite.texture = npc_texture
